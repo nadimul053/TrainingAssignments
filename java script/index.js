aa00a735700be7062f750d45e824ed6c55444549
nadimul053 <148381932+nadimul053@users.noreply.github.com>
2023-11-15 23:44:16 +0600

    java script

---
+++ java script/index.js
@@ -0,0 +1,60 @@
+
+    // var name1 = "Nadimul";
+
+    // document.write( name1 );
+    
+    // var name = "sufia"
+    // var age = "21"
+
+    //  document.write(name)
+    // document.write(age)
+
+
+    // var text = "Bangladesh"
+    
+    // var len = text.length;
+    
+    // document.write ("Number of character" + len);
+
+
+
+    // var text = prompt("Enter your name :")
+
+    // var text = "Bangladesh"
+
+    // document.write(text.charAt(4));
+
+    
+    
+    // var text = "Bangladesh"
+
+    // text = text.toUpperCase();
+
+    // document.write(text);
+
+
+    
+    // var text = "Bangladesh"
+
+    // text = text.toLowerCase();
+
+    // document.write(text);
+
+// {
+    // var text1 = "Bangladesh";
+    // var text2 = " is a beautifull country";
+    // document.write( text1.concat(text2) );
+    
+    // var text1 = "Bangladesh";
+    // var text2 = " is a beautifull country";
+    // var text = text1.concat(text2)
+    // document.write(text);
+// }
+
+
+    // var text1 = "Bangladesh";
+    // var text = text1.slice(0, 4);
+    //var text = text1.slice(0, 4);
+    //document.write(text);
+
+    //  document.getElementById("demo").innerHTML = prompt("Enter your name : ");
